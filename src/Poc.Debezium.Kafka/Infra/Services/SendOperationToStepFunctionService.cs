435f629a6c0b68f5500b099f5ff9affee9b7c0c4
ReenanS <reenan.s.s.rss@gmail.com>
2024-10-20 18:37:42 -0300

    fix: ajustando logs da aplicacao

--- src/Poc.Debezium.Kafka/Infra/Services/SendOperationToStepFunctionService.cs
+++ src/Poc.Debezium.Kafka/Infra/Services/SendOperationToStepFunctionService.cs
@@ -3,6 +3,7 @@ using Amazon.StepFunctions.Model;
 using Domain.Interfaces.UseCases;
 using Domain.Models;
 using Infra.Configurations;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using System.Text.Json;
 
@@ -10,12 +11,12 @@ namespace Infra.Services
 {
     public class SendOperationToStepFunctionService : ISendOperationToStepFunction
     {
-        private readonly ILogger logger;
+        private readonly ILogger<SendOperationToStepFunctionService> logger;
         private readonly IAmazonStepFunctions amazonStepFunctions;
         private readonly KafkaConfiguration kafkaConfiguration;
 
         public SendOperationToStepFunctionService(
-            ILogger logger,
+            ILogger<SendOperationToStepFunctionService> logger,
             IAmazonStepFunctions amazonStepFunctions,
             KafkaConfiguration kafkaConfiguration)
         {
@@ -36,7 +37,7 @@ namespace Infra.Services
                 throw new InvalidOperationException($"Nenhuma State Machine ARN configurada para o tópico {topic}");
             }
 
-            logger.Information("Step Function Input - TipoOperacao: {tipoOperacao}, CodigoExterno: {codigoExterno}, StateMachineArn: {stateMachineArn}", input.TipoOperacao, input.CodigoExterno, stateMachineArn);
+            logger.LogInformation("Step Function Input - TipoOperacao: {tipoOperacao}, CodigoExterno: {codigoExterno}, StateMachineArn: {stateMachineArn}", input.TipoOperacao, input.CodigoExterno, stateMachineArn);
 
             var startExecutionRequest = new StartExecutionRequest
             {
